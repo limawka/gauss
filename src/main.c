c6a555d658ba6aae9bf504651b302b64e4d2df61
unknown <nina.labecka@gmail.com>
2021-01-21 21:56:48 +0100

    selekcja elementu diagonalnego

--- src/main.c
+++ src/main.c
@@ -10,21 +10,32 @@ int main(int argc, char ** argv) {
 	Matrix * A = readFromFile(argv[1]);
 	Matrix * b = readFromFile(argv[2]);
 	Matrix * x;
+	int * q;
 
 	if (A == NULL) return -1;
 	if (b == NULL) return -2;
 	printToScreen(A);
 	printToScreen(b);
+	q = malloc(A->c*sizeof(int));
+	
+	res = eliminate(A,b,q);
 
-	res = eliminate(A,b);
+	if(res == 1){
+		printf("Macierz osobliwa\n");
+		return 1;
+	}else if(res == 2){
+		printf("Zly rozmiar macierzy A - nie jest kwadratowa!\n");
+		return 2;
+	}
 	x = createMatrix(b->r, 1);
+
 	if (x != NULL) {
-		res = backsubst(x,A,b);
+		res = backsubst(x,A,q);
 
 		printToScreen(x);
 	  freeMatrix(x);
 	} else {
-					fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
+		fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
 	}
 
 	freeMatrix(A);
